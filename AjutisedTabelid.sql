922349e9e91b7b92d11a4ea62693675e4e3835ff
adrianapikaljov <pikaljovadri@gmail.com>
2025-10-06 10:30:54 +0300

    Local Temporary tabel on lisatud

---
+++ AjutisedTabelid.sql
@@ -0,0 +1,7 @@
+--34
+--Local Temporary tabel
+create table #PersonDetails1(Id int, Name nvarchar(20))
+insert into #PersonDetails1 values(1, 'Mike')
+insert into #PersonDetails1 values (2, 'John')
+insert into #PersonDetails1 values (3, 'Todd')
+select * from #PersonDetails1
