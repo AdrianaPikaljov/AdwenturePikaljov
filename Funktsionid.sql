8bdf0c2e281c16d9a19d8bd0a9dfac7184ca4fdd
adrianapikaljov <pikaljovadri@gmail.com>
2025-10-06 10:13:48 +0300

    Skaleeritav funktsioon ilma krüpteerimata on lisatud

--- Funktsionid.sql
+++ Funktsionid.sql
@@ -39,3 +39,23 @@ go
 
 --kaivita funktrsiooni 
 select * from fn_MSTVF_GetEmployees()
+
+--33
+
+
+
+--Skaleeritav funktsioon ilma krüpteerimata:
+create function fn_getemployeenamebyId1(@employeekey int)
+returns nvarchar(20)
+as
+begin
+    return (
+        select firstname 
+        from dimemployee 
+        where employeekey = @employeekey
+    )
+end;
+go
+
+--kaivita funktrsiooni 
+select dbo.fn_getemployeenamebyid1(3);
