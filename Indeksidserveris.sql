e20f69630354e9d453bf2100dde6bbe4dd79c726
adrianapikaljov <pikaljovadri@gmail.com>
2025-10-06 12:17:22 +0300

    Unikaalse ja mitteunikaalse indeksi lisamine

--- Indeksidserveris.sql
+++ Indeksidserveris.sql
@@ -52,3 +52,52 @@ on tblEmployee(Gender DESC, Salary ASC)
 --indeksi loomine nime jaoks
 create NonClustered Index IX_tblEmployee_Name
 on tblEmployee(Name)
+
+
+--37
+
+ create table [tblEmployee2]
+ ([Id] int primary key,
+ [FirstName] nvarchar(50),
+ [LastName] nvarchar(50),
+ [Salary] int,
+ [Gender] nvarchar(10),
+ [City] nvarchar(50)
+ )
+
+
+ execute sp_helpindex tblEmployee2
+
+insert into tblEmployee2 values(1, 'Mike','Sandoz', 3100, 'Male', 'New York')
+insert into tblEmployee2 values(1, 'John','Menco', 6500, 'Male', 'London')
+
+drop index tblEmployee2.PK__tblEmplo__3214EC07C89910EF
+
+
+--indeksi loomine veeru Id jaoks
+create clustered Index IX_tblEmployee_Id
+on tblEmployee2(Id)
+
+--esmase võtme kontroll
+execute sp_helpindex tblEmployee2;
+
+--veeru Id indeksi kustutamine
+drop index tblEmployee2.IX_tblEmployee_Id
+
+--unikaalne indeks, mis tagab, et veerus Name 
+--ei esineks korduvaid andmeid
+create unique NonClustered Index UIX_tblEmployee_Name
+on tblEmployee2(FirstName)
+
+--unikaalse piirangu lisamine veerule City
+alter table tblEmployee2
+add constraint UQ_tblEmployee_City
+Unique NonClustered (City)
+
+--indeksi kontroll
+execute sp_helpconstraint tblEmployee2;
+
+--indeks, mis ei luba veerus korduvaid andmeid
+Create unique Index IX_tblEmployee_City
+On tblEmployee2(City)
+with ignore_dup_key
