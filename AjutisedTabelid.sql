efe27ebe7e111cad51d9ad93420916e6611408eb
adrianapikaljov <pikaljovadri@gmail.com>
2025-10-06 10:33:54 +0300

    labivaatus local temporary tabel on loomisele

--- AjutisedTabelid.sql
+++ AjutisedTabelid.sql
@@ -5,3 +5,11 @@ insert into #PersonDetails1 values(1, 'Mike')
 insert into #PersonDetails1 values (2, 'John')
 insert into #PersonDetails1 values (3, 'Todd')
 select * from #PersonDetails1
+
+--sysobjects käsuga TEMPDB alt
+Select name from tempdb..sysobjects
+where name like '#PersonDetails1%'
+
+
+--tabeli kustutamine
+drop table #PersonDetails1
