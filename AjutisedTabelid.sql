f8e172eabb589bd8389dc88c28d59b8e4c4de015
adrianapikaljov <pikaljovadri@gmail.com>
2025-10-06 10:37:35 +0300

    loin salvestatud protseduuri lokaalse ajutise tabeli nimega #PersonDetails1

--- AjutisedTabelid.sql
+++ AjutisedTabelid.sql
@@ -13,3 +13,17 @@ where name like '#PersonDetails1%'
 
 --tabeli kustutamine
 drop table #PersonDetails1
+
+--loome salvestatud protseduuri lokaalse ajutise tabeli nimega #PersonDetails1
+Create procedure spCreateLocalTempTable
+as
+begin
+Create table #PersonDetails1(Id int, Name nvarchar(20))
+insert into #PersonDetails1 values(1, 'Mike')
+insert into #PersonDetails1 values (2, 'John')
+insert into #PersonDetails1 values (3, 'Todd')
+select * from #PersonDetails1
+end
+
+--käivitamine
+exec spCreateLocalTempTable;
